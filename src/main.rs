8106fb1aae2e928efd4fb11a91211752682861f7
nikolaoslibero <nikolaos@violation.studio>
2025-04-25 21:46:09 -0400

    Initialize project structure with Cargo configuration and basic functionality

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use std::{
+    env,
+    io::{self, Write as _},
+};
+
+fn main() -> io::Result<()> {
+    let arguments_and_path = env::args().collect::<Vec<String>>();
+    let Some(arguments) = arguments_and_path.get(1..) else {
+        return Ok(());
+    };
+    let mut stdout = io::stdout();
+
+    stdout.write_all(transform_arguments(arguments).join(" ").as_bytes())?;
+
+    Ok(())
+}
+
+fn transform_arguments(strings: &[String]) -> Vec<String> {
+    strings
+        .iter()
+        .map(|string| transform_argument(string))
+        .collect::<Vec<String>>()
+}
+
+fn transform_argument(argument: &str) -> String {
+    argument.chars().map(swap_character).collect::<String>()
+}
+
+#[expect(clippy::too_many_lines, reason = "PAIRS vec is very long.")]
+fn swap_character(char: char) -> char {
+    const PAIRS: [(char, char); 52] = [
+        ('a', 'e'),
+        ('e', 'i'),
+        ('i', 'o'),
+        ('o', 'u'),
+        ('u', 'a'),
+        ('A', 'E'),
+        ('E', 'I'),
+        ('I', 'O'),
+        ('O', 'U'),
+        ('U', 'A'),
+        ('b', 'c'),
+        ('c', 'd'),
+        ('d', 'f'),
+        ('f', 'g'),
+        ('g', 'h'),
+        ('h', 'j'),
+        ('j', 'k'),
+        ('k', 'l'),
+        ('l', 'm'),
+        ('m', 'n'),
+        ('n', 'p'),
+        ('p', 'q'),
+        ('q', 'r'),
+        ('r', 's'),
+        ('s', 't'),
+        ('t', 'v'),
+        ('v', 'w'),
+        ('w', 'x'),
+        ('x', 'y'),
+        ('y', 'z'),
+        ('z', 'b'),
+        ('B', 'C'),
+        ('C', 'D'),
+        ('D', 'F'),
+        ('F', 'G'),
+        ('G', 'H'),
+        ('H', 'J'),
+        ('J', 'K'),
+        ('K', 'L'),
+        ('L', 'M'),
+        ('M', 'N'),
+        ('N', 'P'),
+        ('P', 'Q'),
+        ('Q', 'R'),
+        ('R', 'S'),
+        ('S', 'T'),
+        ('T', 'V'),
+        ('V', 'W'),
+        ('W', 'X'),
+        ('X', 'Y'),
+        ('Y', 'Z'),
+        ('Z', 'B'),
+    ];
+
+    PAIRS
+        .iter()
+        .find(|&&(input, _)| input == char)
+        .map_or(char, |&(_, output)| output)
+}
